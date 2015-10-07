abfb3d8e47d357bac629f38c1a3cd9d3602d4e62
student <student@asl-desktop>
2015-10-07 16:38:36 +0530

    First Commit

---
+++ my.h
@@ -0,0 +1,20 @@
+
+struct complex {
+	int real;
+	int img;	
+	};
+
+void add();
+void sub();
+void mult();
+void divd();
+void square();
+void sqroot();
+void powr();
+void fact();
+void GCD_LCM();
+void decimal_to_binary();
+int ncr_npr();
+void addn();
+void cmplxnoadr();
+void series();
